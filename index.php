19dd624b2a0f926fa389a5ed2210eb24eeb499b7
Zaphyrzan <wzafirzan@gmail.com>
2021-12-23 09:20:15 +0800

    push before 930

--- index.php
+++ index.php
@@ -3,14 +3,46 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Kenari Travel Agency</title>
 </head>
+
 <body>
     <h1>Kenari Travel Agency</h1>
+    <div class="navbar">
+        <a href="#Home">Home</a>
+        <div class="dropdown">
+            <button class="dropbtn">Service</button>
+            <div class="dropdown-content">
+                <a href="#Japan">Japan</a>
+                <a href="#China">China</a>
+                <a href="#Taiwan">Taiwan</a>
+            </div>
+        </div>
+        <a href="#Contact">Contact Us</a>
+    </div>
     <h4>KL International Airport (KLIA) 64000 KLIA, Sepang Selangor Darul Ehsan, Malaysia</h4>
+    <h2>Travel & Promotion</h2>
+
+    <form action="check.php" method="post">
+        <table>
+            <tr>
+                <td>
+                    <h1>Langkawi Island Of Wonders</h1>
+                    <img src="/images/langkawi.png" alt="langkawi">
+                </td>
+            </tr>
+        </table>
+    </form>
+
+    <div class="copyright">
+        &copy; 2021 All Right Reserved | Kenari Travel Agency
+    </div>
 </body>
+
 </html>
